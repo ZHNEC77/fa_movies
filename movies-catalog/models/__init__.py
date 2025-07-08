274069f861f645b8de2032472d1080f359d35378
ZHNEC77 <vadim030714@gmail.com>
2025-07-08 21:06:34 +0300

    add model movie

--- movies-catalog/models/__init__.py
+++ movies-catalog/models/__init__.py
@@ -2,8 +2,11 @@ __all__ = (
     "Base",
     "DatabaseHelper",
     "db_helper",
+    "Movie",
+
 )
 
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
+from .movie import Movie
